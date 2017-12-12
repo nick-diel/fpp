165dd231bc4cdeb99c39329670924e42cd605cef
Chris Pinkham <cpinkham@bc2va.org>
2017-12-12 14:27:59 -0800

    [ChannelOutput] Bugfix, Re-enable LED panels cape setting for BBB.

--- www/channeloutputs.php
+++ www/channeloutputs.php
@@ -2854,7 +2854,8 @@ tr.rowUniverseDetails td
 								<td><b>Connection:</b></td><td>
 									<select id='LEDPanelsConnection' onChange='LEDPannelsConnectionChanged();'>
 <?
-	if ($settings['Platform'] == "Raspberry Pi")
+	if (($settings['Platform'] == "Raspberry Pi") ||
+		($settings['Platform'] == "BeagleBone Black"))
 	{
 ?>
 										<option value='Hat-Cap-Cape'>Hat/Cap/Cape</option>
